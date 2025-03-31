c73e09d555ab191657e12d1f49220bee9041ab81
sayan4545 <sayanchatterjee3010@gmail.com>
2025-04-01 04:36:10 +0530

    Redis integration for caching

--- Caching-App/src/main/java/com/cachingApp/dtos/EmployeeDto.java
+++ Caching-App/src/main/java/com/cachingApp/dtos/EmployeeDto.java
@@ -2,12 +2,14 @@ package com.cachingApp.dtos;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @Data
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class EmployeeDto {
+public class EmployeeDto implements Serializable {
     private Long id;
     private String email;
     private String name;
